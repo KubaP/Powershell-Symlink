2dde3a5525a01cb3a0b2603275c8f753d7a58653
KubaP <git@kubap.io>
2021-02-27 00:18:46 +0000

    Update build scripts
    - Bring over changes to the vsts-scripts from the TEMPLATE repo.

--- build/vsts-build.ps1
+++ build/vsts-build.ps1
@@ -3,8 +3,8 @@
 	Builds the source code into a module package.
 	
 .DESCRIPTION
-	Compiles all the module source code into a package. All code gets inserted
-	into the main .psm1 file.
+	Compiles all the module source code into a single package. All code gets
+	inserted into the main '.psm1' file.
 	
 .PARAMETER ApiKey
 	The key for publishing to a repository (e.g. PSGallery).
@@ -12,10 +12,10 @@
 .PARAMETER WorkingDirectory
 	The root folder for the whole project, containing the git files, build
 	files, module files, etc.
-  ! If running on Azure, don't specify any value.
+ [!]If running on Azure, don't specify any value.
 	
 .PARAMETER Repository
-	The repository to publish to, by default the PSGallery.
+	The repository to publish to, defaults to 'PSGallery'.
 	
 .PARAMETER TestRepo
 	Publish to the TESTING PSGallery instead.
@@ -75,8 +75,7 @@ param
 # Handle Working Directory paths within Azure pipelines.
 if (-not $WorkingDirectory)
 {
-	if ($env:RELEASE_PRIMARYARTIFACTSOURCEALIAS)
-	{
+	if ($env:RELEASE_PRIMARYARTIFACTSOURCEALIAS) {
 		$WorkingDirectory = Join-Path -Path $env:SYSTEM_DEFAULTWORKINGDIRECTORY -ChildPath $env:RELEASE_PRIMARYARTIFACTSOURCEALIAS
 	}
 	else
@@ -89,27 +88,30 @@ if (-not $WorkingDirectory)
 $ErrorActionPreference = 'Stop'
 
 # Import required modules.
-WriteHeader -Message "Importing PowershellGet Module" -Colour Cyan
+Write-Header -Message "Importing PowershellGet Module" -Colour Cyan
 Import-Module "PowershellGet" -RequiredVersion "2.2.2" -Verbose
-# Print the loaded module information to make it easier for error diagnostics
-# on the azure shell.
+# Print the loaded module information to make it easier for error diagnostics on the azure shell.
 Get-Module -Verbose
 
 # Create the publish folder.
-WriteHeader -Message "Creating and populating publishing directory" -Colour Cyan
-Remove-Item -Path "$WorkingDirectory\publish" -Force -Recurse -ErrorAction 'Continue' | Out-Null
+Write-Header -Message "Creating and populating publishing directory" -Colour Cyan
+# Delete any potentially existing 'publish' folder.
+if (Test-Path "$WorkingDirectory\publish")
+{
+	Remove-Item -Path "$WorkingDirectory\publish" -Force -Recurse | Out-Null
+}
 $publishDir = New-Item -Path $WorkingDirectory -Name "publish" -ItemType Directory -Force -Verbose
 
 # Copy the module files from the root git repository to the publish folder.
-# Only copy the files which don't get "compiled", i.e. the module files
-# themselves, the help documentation, and the xml definitions.
-# The content of the actual code files will get compiled into the module file.
+# Only copy the files which don't get "compiled", i.e. the module files themselves, the help documentation,
+# and the xml definitions.
+# The content of the actual source code files will get compiled into the main module file.
 New-Item -Path $publishDir.FullName -Name "Symlink" -ItemType Directory -Force | Out-Null
 Copy-Item -Path "$WorkingDirectory\Symlink\*" -Destination "$($publishDir.FullName)\Symlink\" `
 	-Recurse -Force -Exclude "tests","internal","functions" -Verbose
 
 # Gather text data from scripts for compilation.
-WriteHeader -Message "Gathering code from function files." -Colour Cyan
+Write-Header -Message "Gathering code from function files." -Colour Cyan
 $text = @()
 $processed = @()
 
@@ -170,9 +172,9 @@ foreach ($line in (Get-Content "$PSScriptRoot\filesAfter.txt" | Where-Object { $
 }
 
 # Update the .psm1 file with all the read-in text content.
-# This is done to reduce load times for the module, if all code is within the 
-# single .psm1 file.
-WriteHeader -Message "Inserting the code into the module file" -Colour Cyan
+# This is done to reduce load times for the module, if all code is within the single .psm1 file.
+# If the code was split across multiple files, the module would take longer when imported.
+Write-Header -Message "Inserting the code into the module file" -Colour Cyan
 $fileData = Get-Content -Path "$($publishDir.FullName)\Symlink\Symlink.psm1" -Raw
 # Change the complied flag to true.
 $fileData = $fileData.Replace('"<was not built>"', '"<was built>"')
@@ -185,7 +187,7 @@ if (-not $SkipPublish)
 	if ($TestRepo)
 	{
 		# Publish to TESTING PSGallery.
-		WriteHeader -Message "TEST Publishing to PSGallery" -Colour Green
+		Write-Header -Message "TEST Publishing to PSGallery" -Colour Green
 		
 		# Register testing repository.
 		Register-PSRepository -Name "test-repo" -SourceLocation "https://www.poshtestgallery.com/api/v2" `
@@ -194,18 +196,18 @@ if (-not $SkipPublish)
 		Publish-Module -Path "$($publishDir.FullName)\Symlink" -NuGetApiKey $ApiKey -Force `
 			-Repository "test-repo" -Verbose
 		
-		WriteHeader -Message "Waiting 60 seconds before testing download" -Colour Green
+		Write-Header -Message "Waiting 60 seconds before testing download" -Colour Green
 		Start-Sleep -Seconds 60
 		
-		# Uninstall the module if it already exists, to then test the
-		# installation of the module from the test PSGallery.
-		WriteHeader -Message "Installing module from PSGallery" -Colour Green
+		# Uninstall the module if it already exists, to then test the installation of the module from the
+		# test PSGallery.
+		Write-Header -Message "Installing module from PSGallery" -Colour Green
 		Uninstall-Module -Name "Symlink" -Force -Verbose -ErrorAction 'Continue'
 		Install-Module -Name "Symlink" -Repository "test-repo" -Force -AcceptLicense -SkipPublisherCheck -Verbose
 		Write-Host "Test Symlink module installed." -ForegroundColor Green
 		
 		# Test if the module has been downloaded and imported correctly.
-		WriteHeader -Message "Importing module" -Colour Green
+		Write-Header -Message "Importing module" -Colour Green
 		Import-Module -Name "Symlink" -Verbose
 		Get-Module -Verbose
 		
@@ -215,7 +217,7 @@ if (-not $SkipPublish)
 	else
 	{
 		# Publish to real repository.
-		WriteHeader -Message "Publishing to $Repository" -Colour Green
+		Write-Header -Message "Publishing to $Repository" -Colour Green
 		Publish-Module -Path "$($publishDir.FullName)\Symlink" -NuGetApiKey $ApiKey -Force `
 			-Repository $Repository -Verbose
 	}
@@ -227,17 +229,16 @@ if (-not $SkipArtifact)
 	$moduleVersion = (Import-PowerShellDataFile -Path "$PSScriptRoot\..\Symlink\Symlink.psd1").ModuleVersion
 	
 	# Move the module contents to a version-labelled subfolder.
-	WriteHeader -Message "Creating Artifact. Moving content to version subfolder" -Colour Magenta
+	Write-Header -Message "Creating Artifact. Moving content to version subfolder" -Colour Magenta
 	New-Item -ItemType Directory -Path "$($publishDir.FullName)\Symlink\" -Name "$moduleVersion" -Force | Out-Null
 	Move-Item -Path "$($publishDir.FullName)\Symlink\*" `
 		-Destination "$($publishDir.FullName)\Symlink\$moduleVersion\" -Exclude "*$moduleVersion*" -Force -Verbose
 	
 	# Create a packaged zip file of the module folder.
-	WriteHeader -Message "Packaging module into archive" -Colour Magenta
+	Write-Header -Message "Packaging module into archive" -Colour Magenta
 	Compress-Archive -Path "$($publishDir.FullName)\Symlink" `
 		-DestinationPath "$($publishDir.FullName)\Symlink-v$($moduleVersion).zip" -Verbose
 	
-	# Write out the module number as a azure pipeline variable for the 
-	# later run publish task.
+	# Write out the module number as a azure pipeline variable for the later run publish task.
 	Write-Host "##vso[task.setvariable variable=version;isOutput=true]$moduleVersion"
 }
--- build/vsts-prerequisites.ps1
+++ build/vsts-prerequisites.ps1
@@ -1,10 +1,11 @@
 ﻿. "$PSScriptRoot\vsts-helpers.ps1"
 
 # Install the required modules for testing.
-WriteHeader -Message "Installing Pester" -Colour Cyan
+Write-Header -Message "Installing Pester" -Colour Cyan
+# Last Pester version 4 before version 5, which is not backwards-compatible.
 Install-Module "Pester" -RequiredVersion "4.10.1" -Force -SkipPublisherCheck -Verbose
 Import-Module "Pester" -Force -PassThru -Verbose
 
-WriteHeader -Message "Installing PSScriptAnalyzer" -Colour Cyan
+Write-Header -Message "Installing PSScriptAnalyzer" -Colour Cyan
 Install-Module "PSScriptAnalyzer" -Force -SkipPublisherCheck -Verbose
 Import-Module "PSScriptAnalyzer" -Force -PassThru -Verbose
